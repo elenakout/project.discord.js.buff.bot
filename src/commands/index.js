2fa6a0e3263aab3a9d9414d056e65a033c12de55
elenakout <elenakout@gmail.com>
2021-01-09 12:35:06 +0200

    stop checking server

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,8 +3,8 @@ const eightBall = require('./8ball');
 const commandsList = require('./commandsList');
 const lol = require('./lol');
 
-const guildID = process.env.GUILD_ID;
-const channelID = process.env.CHANNEL_ID;
+// const guildID = process.env.GUILD_ID;
+// const channelID = process.env.CHANNEL_ID;
 
 const commands = {
   ping,
@@ -14,12 +14,19 @@ const commands = {
 };
 
 module.exports = async (msg) => {
-  if (msg.guild.id === guildID && msg.channel.id === channelID) {
-    const args = msg.content.split(' ');
-    if (args.length == 0 || args[0].charAt(0) !== '!') return;
-    const command = args.shift().substr(1);
-    if (Object.keys(commands).includes(command)) {
-      commands[command](msg, args);
-    }
+  // if (msg.guild.id === guildID && msg.channel.id === channelID) {
+  //   const args = msg.content.split(' ');
+  //   if (args.length == 0 || args[0].charAt(0) !== '!') return;
+  //   const command = args.shift().substr(1);
+  //   if (Object.keys(commands).includes(command)) {
+  //     commands[command](msg, args);
+  //   }
+  // }
+
+  const args = msg.content.split(' ');
+  if (args.length == 0 || args[0].charAt(0) !== '!') return;
+  const command = args.shift().substr(1);
+  if (Object.keys(commands).includes(command)) {
+    commands[command](msg, args);
   }
 };
